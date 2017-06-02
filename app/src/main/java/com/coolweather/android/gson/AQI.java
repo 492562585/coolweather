18fc0a652ccd4622ea4356a47cd20d96094b92f4
tianyanyu <492562585@qq.com>
2017-06-02 21:06:44 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by tianyanyu on 2017/6/1 0001.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
